755509ab634d1f05b1abb104a6f1dc8175732356
Vivek Saradva <129260694+confused1439@users.noreply.github.com>
2024-07-20 22:13:21 +0530

    Update AuthPage.jsx

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -6,7 +6,7 @@ const AuthPage = (props) => {
         const {value} = e.target[0];
 
        axios.post(
-            'http://localhost:3000/authenticate',
+            'https://node-js-react-js-chatapp-api.vercel.app/authenticate',
             {username: value}
         )
         .then(r => props.onAuth({...r.data, secret: value}))
